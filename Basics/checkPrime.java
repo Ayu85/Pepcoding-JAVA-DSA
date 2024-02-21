cb413d1092b1ffdad46c3778f0d1bad4c77b5e54
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-21 08:11:59 +0000

    optimal

--- Basics/checkPrime.java
+++ Basics/checkPrime.java
@@ -6,9 +6,16 @@ public class checkPrime {
         }
         return true;
     }
+    static boolean checkOptimal(int n) {
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
-        System.out.println(check(5));
-
+        System.out.println(checkOptimal(97));
+        
     }
 }
