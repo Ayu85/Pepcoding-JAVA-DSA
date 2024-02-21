545c86c20845b48e1b965e313960ccc468016ad1
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-21 07:57:18 +0000

    checkprime done

--- Basics/checkPrime.java
+++ Basics/checkPrime.java
@@ -1,3 +1,14 @@
-public class checkPrime{
-    
-} 
+public class checkPrime {
+    static boolean check(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(check(5));
+
+    }
+}
