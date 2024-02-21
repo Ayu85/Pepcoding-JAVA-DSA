60725049dcfae308b92a30431f63fc1f40442bf1
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-21 09:31:43 +0000

    optimal done

--- Basics/countDigits.java
+++ Basics/countDigits.java
@@ -1,13 +1,16 @@
 public class countDigits {
-    static int getDigits(int num){
-        int digCount=0;
-        while(num>0){
+    static int getDigits(int num) {
+        int digCount = 0;
+        while (num > 0) {
+            int rem = num % 10;
+            System.out.print(rem+" ");
             digCount++;
-            num/=10;
+            num /= 10;
         }
         return digCount;
     }
-    public static void main(String[] args){
-        System.out.println(getDigits(25));
+
+    public static void main(String[] args) {
+        System.out.println("digits count "+getDigits(25));
     }
 }
