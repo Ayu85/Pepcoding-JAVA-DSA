b18c53fffaaf9e038506137e9e0dc584f597fc63
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-22 07:43:23 +0000

    got the approach

--- Basics/rotateNum.java
+++ Basics/rotateNum.java
@@ -1,3 +1,21 @@
 public class rotateNum {
-    
+    static int rotate(int num, int k) {
+        int result = 0;
+        int pow=k;
+
+        while (k>0) {
+            int rem = num % 10;
+            result += rem * Math.pow(10, pow-1);
+            num /= 10;
+            pow++;
+            k--;
+            System.out.println(k);
+            i++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rotate(23457, 3));
+    }
 }
