1734f22caca64368d502400197077995d200d02b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 04:05:24 +0000

    printed

---
+++ Arrays/barChart.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class barChart {
+    static void printBar(int[] input) {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 1; j <= input[i]; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+          
+        }
+    }
+
+    public static void main(String[] args) {
+        printBar(new int[] { 1, 4, 2, 5 });
+    }
+}
