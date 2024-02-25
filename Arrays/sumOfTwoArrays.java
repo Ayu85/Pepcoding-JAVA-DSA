25b44eb70933360e9f3a6b186a6eb31cc9ca849b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 05:01:06 +0000

    array with different lengths

--- Arrays/sumOfTwoArrays.java
+++ Arrays/sumOfTwoArrays.java
@@ -9,15 +9,16 @@ public class sumOfTwoArrays {
         int[] sum = new int[n1 > n2 ? n1 : n2];
         int i = n1 - 1;
         int j = n2 - 1;
+        int k=sum.length-1;
         while (i >= 0 && j >= 0) {
-            sum[i] = arr1[i--] + arr2[j--];
+            sum[k--] = arr1[i--] + arr2[j--];
         }
         return sum;
     }
 
     public static void main(String[] args) {
         int[] a = { 1, 2, 3, 4 };
-        int[] b = { 1, 2, 3, 4 };
+        int[] b = { 9, 1, 2, 3, 4 };
         System.out.println(Arrays.toString(getSum2(a, b, a.length, b.length)));
     }
 }
