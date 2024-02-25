120214aefcb5c1fb4ac2ef8e6a7d5aab4e1c63be
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 19:55:33 +0530

    fucking did it

---
+++ Stacks/src/DuplicateBrackets.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class DuplicateBrackets {
+    static boolean checkDuplicates(String s) {
+        Stack<Character> stk = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ')')
+                stk.push(s.charAt(i));
+            else {
+                while (stk.peek() != '(') {
+                    stk.pop();
+                }
+                if (stk.peek() == '(')
+                    stk.pop();
+            }
+        }
+        return !stk.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String exp = "((a+b)+(c+d))";
+        String exp2 = "(a+b)+((c+d))";
+        System.out.println(checkDuplicates(exp2));
+    }
+}
