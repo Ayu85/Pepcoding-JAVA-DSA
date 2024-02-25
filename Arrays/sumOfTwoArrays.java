7ae1e5fa79398f2f5f4bf64b915a811bf308cdd7
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 04:58:30 +0000

    same length array working

--- Arrays/sumOfTwoArrays.java
+++ Arrays/sumOfTwoArrays.java
@@ -3,10 +3,14 @@ package Arrays;
 import java.util.Arrays;
 
 public class sumOfTwoArrays {
-    static int[] getSum(int[] arr1, int[] arr2, int n1, int n2) {
+    // static int[] getSum(int[] arr1, int[] arr2, int n1, int n2) {
+    // }
+    static int[] getSum2(int[] arr1, int[] arr2, int n1, int n2) {
         int[] sum = new int[n1 > n2 ? n1 : n2];
-        for (int i = 0; i < n1; i++) {
-            sum[i] = arr1[i] + arr2[i];
+        int i = n1 - 1;
+        int j = n2 - 1;
+        while (i >= 0 && j >= 0) {
+            sum[i] = arr1[i--] + arr2[j--];
         }
         return sum;
     }
@@ -14,6 +18,6 @@ public class sumOfTwoArrays {
     public static void main(String[] args) {
         int[] a = { 1, 2, 3, 4 };
         int[] b = { 1, 2, 3, 4 };
-        System.out.println(Arrays.toString(getSum(a, b, a.length, b.length)));
+        System.out.println(Arrays.toString(getSum2(a, b, a.length, b.length)));
     }
 }
