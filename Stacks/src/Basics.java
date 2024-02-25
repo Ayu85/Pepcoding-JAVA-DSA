a99032afb1ea6de527da60be24b75becf0ab51c9
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 11:27:29 +0530

    stacks basics

--- Stacks/src/Basics.java
+++ Stacks/src/Basics.java
@@ -3,6 +3,10 @@ public class Basics {
     public static void main(String[] args){
         Stack<Integer> st=new Stack<>();
         st.push(10);
+        st.push(20);
+        st.pop();
+        st.pop();
+
         System.out.println(st);
     }
 }
