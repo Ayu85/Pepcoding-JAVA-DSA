96251835203eef9d6dd3b858c4d03b1f9fb99c11
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-25 11:26:19 +0530

    stacks

---
+++ Stacks/src/Basics.java
@@ -0,0 +1,8 @@
+import java.util.Stack;
+public class Basics {
+    public static void main(String[] args){
+        Stack<Integer> st=new Stack<>();
+        st.push(10);
+        System.out.println(st);
+    }
+}
