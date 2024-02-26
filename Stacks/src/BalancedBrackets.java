42026711e862b7e5651e7b619e88c5e6fa89040f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-26 11:19:16 +0530

    balanced brackets test cases passed

--- Stacks/src/BalancedBrackets.java
+++ Stacks/src/BalancedBrackets.java
@@ -2,11 +2,30 @@ import java.util.Stack;
 
 public class BalancedBrackets {
     static boolean isBalanced(String str) {
-        Stack<Integer> stk = new Stack<>();
+        Stack<Character> stk = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(' || str.charAt(i) == '{' || str.charAt(i) == '[') {
+                stk.push(str.charAt(i));
+            } else if (str.charAt(i) == ')') {
+                if (stk.isEmpty()) return false; //means extra bracket is there
+                else if (stk.peek() != '(') return false; //means mismatch of brackets
+                else stk.pop(); //otherwise pop
+            } else if (str.charAt(i) == '}') {
+                if (stk.isEmpty()) return false;
+                else if (stk.peek() != '{') return false;
+                else stk.pop();
+            } else if (str.charAt(i) == ']') {
+                if (stk.isEmpty()) return false;
+                else if (stk.peek() != '[') return false;
+                else stk.pop();
+            }
+
+        }
+        return stk.isEmpty();
     }
 
     public static void main(String[] args) {
-
+        System.out.println(isBalanced("[(])"));
     }
 
 }
