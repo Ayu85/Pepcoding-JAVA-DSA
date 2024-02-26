19c06fb42d3576ea028df984a7759511e70bbd9d
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-26 15:17:04 +0530

    brute force done

---
+++ Stacks/src/nextGreaterElement.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class nextGreaterElement {
+    static Stack getGreaterElements(int[] arr) {
+        Stack<Integer> stk = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            int max = -1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > arr[i]) {
+                    max = arr[j];
+                    break;
+                }
+            }
+            stk.push(max);
+        }
+        return stk;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getGreaterElements(new int[]{2, 5, 9, 3, 1, 12,6, 8, 7}));
+    }
+}
