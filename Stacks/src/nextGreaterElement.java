4f58ee8c94dcbff27e67017af87430957acb2693
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-26 16:19:02 +0530

    optimal

--- Stacks/src/nextGreaterElement.java
+++ Stacks/src/nextGreaterElement.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Stack;
 
 public class nextGreaterElement {
@@ -16,7 +17,27 @@ public class nextGreaterElement {
         return stk;
     }
 
+    static int[] getGreaterOptimal(int[] arr) {
+        Stack<Integer> stk = new Stack<>();
+        int[] temp = new int[arr.length];
+        temp[temp.length - 1] = -1;
+        stk.push(arr[arr.length - 1]);
+        int k = temp.length - 2;
+        for (int i = arr.length - 2; i >= 0; i--) {
+            while (!stk.isEmpty() && stk.peek() <= arr[i])
+                stk.pop();
+            if (stk.isEmpty())
+                temp[k--] = -1;
+            else
+                temp[k--] = stk.peek();
+
+
+            stk.push(arr[i]);
+        }
+        return temp;
+    }
+
     public static void main(String[] args) {
-        System.out.println(getGreaterElements(new int[]{2, 5, 9, 3, 1, 12,6, 8, 7}));
+        System.out.println(Arrays.toString(getGreaterOptimal(new int[]{2, 5, 9, 3, 1, 12, 6, 8, 7})));
     }
 }
