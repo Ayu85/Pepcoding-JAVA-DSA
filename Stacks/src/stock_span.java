53f42b2ab2953569ad0bcc3dd0e278101fc91977
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-27 11:05:55 +0530

    brute force done

--- Stacks/src/stock_span.java
+++ Stacks/src/stock_span.java
@@ -7,15 +7,22 @@ public class stock_span {
         for (int i = n - 1; i >= 0; i--) {
             int count = 0;
             int max = arr[i];
+            int flag = 0;
             for (int j = i - 1; j >= 0; j--) {
                 if (arr[j] > max) {
                     max = arr[j];
                     count++;
                     break;
+                } else if (arr[j] == max) {
+                    flag = 1;
+                    count++;
+                    break;
                 }
                 count++;
             }
-            if (max == arr[i])
+            if (flag == 1)
+                temp[k--] = count;
+            else if (max == arr[i])
                 temp[k--] = i + 1;
             else
                 temp[k--] = count;
@@ -25,6 +32,8 @@ public class stock_span {
 
     public static void main(String[] args) {
         int[] arr = {60, 70, 80, 100, 90, 75, 80, 120};
-        System.out.println(Arrays.toString(getSpan(arr, arr.length)));
+        int[] arr2 = {5, 3, 8, 7, 10, 7, 7, 12, 16};
+
+        System.out.println(Arrays.toString(getSpan(arr2, arr2.length)));
     }
 }
