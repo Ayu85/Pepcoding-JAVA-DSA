94ce1752a2b977f657f0201546ecec72e89a32dc
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-01 08:50:24 +0530

    optimal

---
+++ Stacks/Practice/src/BalancedBrackets.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+public class BalancedBrackets {
+    static boolean checkBalanced(String expr) {
+        Stack<Character> stk = new Stack<>();
+        for (int i = 0; i < expr.length(); i++) {
+            char ch = expr.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stk.push(ch);
+            } else if (ch == ')') {
+                if (stk.peek() != '(') return false;
+                else
+                    stk.pop();
+            } else if (ch == '}') {
+                if (stk.peek() != '{') return false;
+                else
+                    stk.pop();
+            } else if (ch == ']') {
+                if (stk.peek() != '[') return false;
+                else
+                    stk.pop();
+            }
+        }
+        return stk.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s1 = "[(a+b)+{(c+d)*(e/f)}";
+        System.out.println(checkBalanced(s1));
+    }
+}
