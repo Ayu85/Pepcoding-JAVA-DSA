583c64cabba30c28cc414e0e76b8b416a77ef758
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-03 08:58:54 +0530

    all test cases passed

---
+++ Stacks/src/nextSmallerToLeft.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class nextSmallerToLeft {
+    static int[] getSmaller(int[] arr, int n) {
+        Stack<Integer> stk = new Stack<>();
+        int[] temp = new int[n];
+        stk.push(arr[0]);
+        temp[0] = -1;
+        for (int i = 1; i < n; i++) {
+            if (!stk.isEmpty() && stk.peek() < arr[i])
+                temp[i] = stk.peek();
+            else if (stk.isEmpty())
+                temp[i] = -1;
+            else {
+                while (!stk.isEmpty() && stk.peek() > arr[i])
+                    stk.pop();
+                temp[i] = stk.isEmpty() ? -1 : stk.peek();
+            }
+            stk.push(arr[i]);
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(getSmaller(new int[]{4, 5, 2, 10, 8}, 5)));
+    }
+}
