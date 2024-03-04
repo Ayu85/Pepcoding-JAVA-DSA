b9980f8098ab193e73dbf84cd5f7d0ebc42ea6e7
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-04 19:12:28 +0530

    next smaller to right

--- Stacks/src/nextSmallerToRight.java
+++ Stacks/src/nextSmallerToRight.java
@@ -1,5 +1,23 @@
+import java.util.*;
+
 public class nextSmallerToRight {
     static int[] getSmaller(int[] arr, int n) {
-
+        Stack<Integer> stk = new Stack<>();
+        int[] result = new int[n];
+        stk.push(arr[n - 1]);
+        result[n - 1] = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (!stk.isEmpty() && stk.peek() < arr[i])
+                result[i] = stk.peek();
+            else if (stk.isEmpty())
+                result[i] = -1;
+            else {
+                while (!stk.isEmpty() && stk.peek() > arr[i])
+                    stk.pop();
+                result[i] = stk.isEmpty() ? -1 : stk.peek();
+            }
+            stk.push(arr[i]);
+        }
+        return result;
     }
 }
