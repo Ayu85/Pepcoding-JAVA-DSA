e419459f1daae0a29e3b2315bc63e9e62168ee40
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-05 08:40:59 +0530

    first test case passed

--- Stacks/src/stockSpan.java
+++ Stacks/src/stockSpan.java
@@ -1,3 +1,26 @@
-public class stockSpan {
+import java.util.*;
 
+public class stockSpan {
+    static int[] getStockSpan(int[] arr, int n) {
+        Stack<Integer> stk = new Stack<>();
+        int[] temp = new int[n];
+        stk.push(0);
+        temp[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[stk.peek()] < arr[i]) {
+                while (arr[stk.peek()] < arr[i]) {
+                    stk.pop();
+                    temp[i] = i - stk.peek();
+                }
+            } else {
+                temp[i] = 1;
+            }
+            stk.push(i);
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(getStockSpan(new int[]{100, 80, 60, 70, 60, 75, 85}, 7)));
+    }
 }
