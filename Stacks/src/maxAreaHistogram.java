b2aa9624144cd512f6b3c4e03b47f9b6e48451e9
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-06 19:16:04 +0530

    first test case passed

--- Stacks/src/maxAreaHistogram.java
+++ Stacks/src/maxAreaHistogram.java
@@ -1,13 +1,34 @@
 public class maxAreaHistogram {
+    static int getNSL(int[] arr, int index) {
+        for (int i = index - 1; i >= 0; i--) {
+            if (arr[i] < arr[index])
+                return i;
+        }
+        return 0;
+    }
+
+    static int getNSR(int[] arr, int index) {
+        for (int i = index + 1; i < arr.length; i++) {
+            if (arr[i] < arr[index])
+                return i;
+        }
+        return 0;
+    }
+
     static int getMaxArea(int[] arr, int n) {
-        int maxArea=-1;
+        int maxArea = -1;
         for (int i = 0; i < n; i++) {
-            int ngl = getNGL(arr, arr[i]);
-            int ngr = getNGR(arr, arr[i]);
-            int area=(ngr-ngl)*arr[i];
-            if(area>maxArea)
-                maxArea=area;
+            int nsl = getNSL(arr, i);
+            int nsr = getNSR(arr, i);
+            int area = (nsr - nsl-1) * arr[i];
+            if (area > maxArea)
+                maxArea = area;
         }
         return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {6, 2, 5, 4, 5, 1, 6};
+        System.out.println(getMaxArea(arr,arr.length));
     }
 }
